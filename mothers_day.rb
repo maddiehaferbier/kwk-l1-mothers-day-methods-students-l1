ab91277d71fc8684fd3b158eb07ee44ffa9e1f71
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 18:12:39 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,5 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
+"happy mothers day"
 
 
 ## Call your method below - use puts to see it in the terminal!
